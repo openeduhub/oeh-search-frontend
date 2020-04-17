c4337e6e79619e633489eb32f7084c41fd8a4f66
Christopher Lübbemeier <luebbemeier@metaventis.com>
2020-04-17 17:27:27 +0200

    minor html updates in details and headerbar

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -8,23 +8,11 @@ import { Result } from 'shared/types';
     styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
-
-    groups = ['discipline', 'educationalContext', 'keywords']
-
     details: Result;
 
     constructor(private route: ActivatedRoute) {}
 
     ngOnInit(): void {
         this.route.data.subscribe((data: { details: Result }) => (this.details = data.details));
-    }
-    getGroupData(group): string[] {
-        if(group === 'keywords') {
-            return this.details.lom.general.keyword;
-        }
-        return this.details?.valuespaces[group]?.map((data) => data.de);
-    }
-    openSource(): void {
-        window.location.href = this.details.lom.technical.location;
     }
 }
