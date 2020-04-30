7adf5d58c54b5ccbee5065f817e475486960b257
Christopher Lübbemeier <luebbemeier@metaventis.com>
2020-04-30 10:57:18 +0200

    move facet logic to filter component

--- src/app/search-filterbar/search-filterbar.component.ts
+++ src/app/search-filterbar/search-filterbar.component.ts
@@ -1,17 +1,17 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Facets, Filters } from '../search.service';
+import { Facets, Filters, SearchService } from '../search.service';
 import { SortPipe } from '../sort.pipe';
+import { filter, first } from 'rxjs/operators';
 
 @Component({
     selector: 'app-search-filterbar',
     templateUrl: './search-filterbar.component.html',
     styleUrls: ['./search-filterbar.component.scss'],
 })
-export class SearchFilterbarComponent implements OnInit, OnChanges {
-    @Input() facets: Facets;
-
+export class SearchFilterbarComponent implements OnInit {
+    facets: Facets;
     show = false;
     filters: Filters = {};
     facetFilters: FormGroup;
@@ -21,15 +21,13 @@ export class SearchFilterbarComponent implements OnInit, OnChanges {
         private formBuilder: FormBuilder,
         private route: ActivatedRoute,
         private router: Router,
+        private search: SearchService,
     ) {}
 
-    ngOnChanges(changes: SimpleChanges) {
-        if (changes.facets) {
-            this.initFacetFilters(changes.facets.currentValue);
-        }
-    }
-
     ngOnInit(): void {
+        // Any user input that affects results is funneled through query params. On any such input,
+        // we call `router.navigate`. The rest is handled by `onQueryParams`, both, when initially
+        // loading and when updating the page.
         this.route.queryParams.subscribe((params) => {
             if (params.filters) {
                 this.filters = JSON.parse(params.filters);
@@ -42,6 +40,15 @@ export class SearchFilterbarComponent implements OnInit, OnChanges {
                 // If `facetFilters` are not yet initialized, this will be done on initialization.
             }
         });
+
+        this.search.getFacets().subscribe((facets) => this.updateFacets(facets));
+        this.search
+            .getFacets()
+            .pipe(
+                filter((facets) => facets !== null),
+                first(),
+            )
+            .subscribe((facets) => this.initFacetFilters(facets));
     }
 
     toggle(key: string) {
@@ -57,6 +64,7 @@ export class SearchFilterbarComponent implements OnInit, OnChanges {
             values: ['NONE', 'MIXED', 'ALL'],
         });
     }
+
     // does not transfer via template
     getTypes() {
         return new SortPipe().transform(this.facets.types.buckets, {
@@ -65,8 +73,12 @@ export class SearchFilterbarComponent implements OnInit, OnChanges {
         });
     }
 
+    /**
+     * Initialize visual representation of facet filters.
+     *
+     * Call only once.
+     */
     private initFacetFilters(facets: Facets) {
-        console.log(facets);
         this.facetFilters = this.formBuilder.group(
             // Create a new object with every key of `facets` mapped to an empty array. This will
             // create a new form control for each facet with nothing selected.
@@ -83,5 +95,24 @@ export class SearchFilterbarComponent implements OnInit, OnChanges {
                 queryParamsHandling: 'merge',
             });
         });
+    }
+
+    private updateFacets(facets: Facets) {
+        if (!facets) {
+            return;
+        }
+        if (!this.facets) {
+            this.facets = {} as Facets;
+        }
+        for (const [label, facet] of Object.entries(facets)) {
+            // Leave the facet object in place if it already exists, so Angular won't reconstruct
+            // the whole thing and the select dialog won't close every time the user selects an
+            // option.
+            if (this.facets[label]) {
+                this.facets[label].buckets = facet.buckets;
+            } else {
+                this.facets[label] = facet;
+            }
+        }
     }
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { ActivatedRoute, Params, Router } from '@angular/router';
-import { filter, first } from 'rxjs/operators';
 import { DidYouMeanSuggestionFragment } from 'src/generated/graphql';
-import { Facets, SearchService, Filters } from '../search.service';
+import { Facets, SearchService } from '../search.service';
 
 @Component({
     selector: 'app-search',
@@ -14,52 +11,13 @@ export class SearchComponent implements OnInit {
     facets: Facets;
     didYouMeanSuggestion: DidYouMeanSuggestionFragment;
 
-    constructor(
-        private route: ActivatedRoute,
-        private formBuilder: FormBuilder,
-        private router: Router,
-        private search: SearchService,
-    ) {}
+    constructor(private search: SearchService) {}
 
     ngOnInit(): void {
-        // Any user input that affects results is funneled through query params. On any such input,
-        // we call `router.navigate`. The rest is handled by `onQueryParams`, both, when initially
-        // loading and when updating the page.
-        this.registerSearchObservers();
-    }
-
-    private registerSearchObservers() {
-        this.search.getFacets().subscribe((facets) => this.setFacets(facets));
-        this.search
-            .getFacets()
-            .pipe(
-                filter((facets) => facets !== null),
-                first(),
-            )
-            .subscribe((facets) => this.facets = facets);
         this.search
             .getDidYouMeanSuggestion()
             .subscribe(
                 (didYouMeanSuggestion) => (this.didYouMeanSuggestion = didYouMeanSuggestion),
             );
-    }
-
-    private setFacets(facets: Facets) {
-        if (!facets) {
-            return;
-        }
-        if (!this.facets) {
-            this.facets = {} as Facets;
-        }
-        for (const [label, facet] of Object.entries(facets)) {
-            // Leave the facet object in place if it already exists, so Angular won't reconstruct
-            // the whole thing and the select dialog won't close every time the user selects an
-            // option.
-            if (this.facets[label]) {
-                this.facets[label].buckets = facet.buckets;
-            } else {
-                this.facets[label] = facet;
-            }
-        }
     }
 }
