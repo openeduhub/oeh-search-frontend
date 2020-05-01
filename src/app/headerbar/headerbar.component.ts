44e031efb73b3a29f24276ac7687b4ee88c93edf
Torsten Simon <simon@edu-sharing.net>
2020-05-01 13:01:53 +0200

    mobile layout fixes, ui improvements

--- src/app/headerbar/headerbar.component.ts
+++ src/app/headerbar/headerbar.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-headerbar',
     templateUrl: './headerbar.component.html',
     styleUrls: ['./headerbar.component.scss'],
 })
-export class HeaderbarComponent {}
+export class HeaderbarComponent {
+    filter: boolean;
+    filterCount: number;
+    constructor(private route: ActivatedRoute) {
+        this.route.queryParams.subscribe((params) => {
+            this.filter = params.filter === 'true';
+            this.filterCount = 0;
+            if (params.filters) {
+                const filters = JSON.parse(params.filters);
+                this.filterCount = Object.keys(filters).filter((k) => filters[k]?.length).length;
+            }
+        });
+    }
+}
