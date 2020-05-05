7d658e9c25ec1e17be91863f445cb192a30138f8
Christopher Lübbemeier <luebbemeier@metaventis.com>
2020-05-05 12:53:46 +0200

    make filter bar span screen on mobile

--- src/app/search-filterbar/search-filterbar.component.ts
+++ src/app/search-filterbar/search-filterbar.component.ts
@@ -1,21 +1,23 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter, first } from 'rxjs/operators';
 import { Facets, Filters, SearchService } from '../search.service';
 import { SortPipe } from '../sort.pipe';
-import { filter, first } from 'rxjs/operators';
 
 @Component({
     selector: 'app-search-filterbar',
     templateUrl: './search-filterbar.component.html',
     styleUrls: ['./search-filterbar.component.scss'],
 })
-export class SearchFilterbarComponent implements OnInit {
+export class SearchFilterbarComponent implements OnInit, OnDestroy {
     facets: Facets;
-    show = false;
     filters: Filters = {};
     facetFilters: FormGroup;
     expanded = ['disciplines', 'educationalContexts'];
+
+    private subscriptions: Subscription[] = [];
 
     constructor(
         private formBuilder: FormBuilder,
@@ -28,20 +30,25 @@ export class SearchFilterbarComponent implements OnInit {
         // Any user input that affects results is funneled through query params. On any such input,
         // we call `router.navigate`. The rest is handled by `onQueryParams`, both, when initially
         // loading and when updating the page.
-        this.route.queryParams.subscribe((params) => {
-            if (params.filters) {
-                this.setFilters(JSON.parse(params.filters));
-            } else {
-                this.setFilters({});
-            }
-            this.show = params.filter === 'true';
-            if (this.facetFilters) {
-                this.facetFilters.reset(this.filters, { emitEvent: false });
-                // If `facetFilters` are not yet initialized, this will be done on initialization.
-            }
-        });
+        this.subscriptions.push(
+            this.route.queryParams.subscribe((params) => {
+                if (params.filters) {
+                    this.setFilters(JSON.parse(params.filters));
+                } else {
+                    this.setFilters({});
+                }
+                if (this.facetFilters) {
+                    this.facetFilters.reset(this.filters, { emitEvent: false });
+                    // If `facetFilters` are not yet initialized, this will be
+                    // done on initialization.
+                }
+            }),
+        );
 
-        this.search.getFacets().subscribe((facets) => this.updateFacets(facets));
+        this.subscriptions.push(
+            this.search.getFacets().subscribe((facets) => this.updateFacets(facets)),
+        );
+        // Don't need to unsubscribe since subscription completes after first value.
         this.search
             .getFacets()
             .pipe(
@@ -49,6 +56,12 @@ export class SearchFilterbarComponent implements OnInit {
                 first(),
             )
             .subscribe((facets) => this.initFacetFilters(facets));
+    }
+
+    ngOnDestroy(): void {
+        for (const subscription of this.subscriptions) {
+            subscription.unsubscribe();
+        }
     }
 
     toggle(key: string) {
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,23 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DidYouMeanSuggestionFragment } from 'src/generated/graphql';
 import { Facets, SearchService } from '../search.service';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-search',
     templateUrl: './search.component.html',
     styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, OnDestroy {
     facets: Facets;
     didYouMeanSuggestion: DidYouMeanSuggestionFragment;
+    showFilterBar = false;
 
-    constructor(private search: SearchService) {}
+    private subscriptions: Subscription[] = [];
+
+    constructor(private search: SearchService, private route: ActivatedRoute) {}
 
     ngOnInit(): void {
-        this.search
-            .getDidYouMeanSuggestion()
-            .subscribe(
-                (didYouMeanSuggestion) => (this.didYouMeanSuggestion = didYouMeanSuggestion),
-            );
+        this.subscriptions.push(
+            this.search
+                .getDidYouMeanSuggestion()
+                .subscribe(
+                    (didYouMeanSuggestion) => (this.didYouMeanSuggestion = didYouMeanSuggestion),
+                ),
+        );
+        this.subscriptions.push(
+            this.route.queryParams.subscribe((params) => {
+                this.showFilterBar = params.filter === 'true';
+            }),
+        );
+    }
+
+    ngOnDestroy(): void {
+        for (const subscription of this.subscriptions) {
+            subscription.unsubscribe();
+        }
     }
 }
