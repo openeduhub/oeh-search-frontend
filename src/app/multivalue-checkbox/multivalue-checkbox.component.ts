d1a9108195d916d152e9683d7e3c5e818d0f2be0
Christopher Lübbemeier <luebbemeier@metaventis.com>
2020-05-15 16:28:12 +0200

    format multivalue-checkbox.component.ts

--- src/app/multivalue-checkbox/multivalue-checkbox.component.ts
+++ src/app/multivalue-checkbox/multivalue-checkbox.component.ts
@@ -4,16 +4,16 @@ import { Bucket } from '../../generated/graphql';
 import { MatCheckboxChange } from '@angular/material/checkbox';
 
 @Component({
-  selector: 'app-multivalue-checkbox',
-  templateUrl: './multivalue-checkbox.component.html',
-  styleUrls: ['./multivalue-checkbox.component.scss'],
-  providers: [
-      {
-          provide: NG_VALUE_ACCESSOR,
-          multi: true,
-          useExisting: forwardRef(() => MultivalueCheckboxComponent),
-      }
-  ]
+    selector: 'app-multivalue-checkbox',
+    templateUrl: './multivalue-checkbox.component.html',
+    styleUrls: ['./multivalue-checkbox.component.scss'],
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            multi: true,
+            useExisting: forwardRef(() => MultivalueCheckboxComponent),
+        },
+    ],
 })
 export class MultivalueCheckboxComponent implements ControlValueAccessor {
     @Input() options: Bucket[];
@@ -21,19 +21,19 @@ export class MultivalueCheckboxComponent implements ControlValueAccessor {
     @Input() orientation: 'vertical' | 'horizontal' = 'vertical';
     values: string[] = [];
 
-    constructor() { }
+    constructor() {}
     propagateChange = (_: any) => {};
 
     registerOnChange(fn: (_: any) => {}): void {
         this.propagateChange = fn;
     }
 
-    registerOnTouched(fn: (_: any) => {}): void {
-    }
+    registerOnTouched(fn: (_: any) => {}): void {}
 
     writeValue(obj: any): void {
-      this.values = obj || [];
+        this.values = obj || [];
     }
+
     toggleValue(key: string, value: MatCheckboxChange) {
         if (value.checked) {
             this.values.push(key);
@@ -42,5 +42,4 @@ export class MultivalueCheckboxComponent implements ControlValueAccessor {
         }
         this.propagateChange(this.values);
     }
-
 }
