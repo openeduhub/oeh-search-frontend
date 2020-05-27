c8b4d7a2c37050b2f658cfb28c340b490c941949
grohmio <ag17@gmx.de>
2020-05-27 14:40:35 +0200

    fix review offences

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    relayUrl: 'https://staging.wirlernenonline.de/relay/graphql',
-    editorBackendUrl: 'https://staging.wirlernenonline.de/editor/graphql',
+    relayUrl: 'https://staging.wirlernenonline.de/relay',
+    editorBackendUrl: 'https://staging.wirlernenonline.de/editor',
     openId: {
         issuer: 'https://idm.wirlernenonline.de/auth/realms/master',
         clientId: 'oeh-search-frontend',
