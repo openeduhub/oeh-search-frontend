4f389a78140b2d7135a9feb12faa0c8e84bfdbed
Andreas Grohmann <ag17@gmx.de>
2020-05-27 14:56:17 +0200

    add stage builds in preparation for ci pipeline and e2e tests (#6)

---
+++ src/environments/environment.stage.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    relayUrl: 'https://staging.wirlernenonline.de/relay',
+    editorBackendUrl: 'https://staging.wirlernenonline.de/editor',
+    openId: {
+        issuer: 'https://idm.wirlernenonline.de/auth/realms/master',
+        clientId: 'oeh-search-frontend',
+    },
+};
