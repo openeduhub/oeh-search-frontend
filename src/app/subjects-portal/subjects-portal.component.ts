c8ab73cec53a89732ff00b9753624904f6957143
Christopher Lübbemeier <luebbemeier@metaventis.com>
2020-06-02 12:29:00 +0200

    move to MatExpansionPanel for subjects portal

--- src/app/subjects-portal/subjects-portal.component.ts
+++ src/app/subjects-portal/subjects-portal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SubjectsPortalResults } from '../subjects-portal-resolver.service';
+import { parseSearchQueryParams } from '../utils';
 
 @Component({
     selector: 'app-subjects-portal',
@@ -9,13 +10,21 @@ import { SubjectsPortalResults } from '../subjects-portal-resolver.service';
 })
 export class SubjectsPortalComponent implements OnInit {
     results: SubjectsPortalResults;
-    isExpanded = true;
+    isExpanded: boolean;
 
     constructor(private route: ActivatedRoute) {}
 
     ngOnInit(): void {
         this.route.data.subscribe((data: { subjectsPortalResults: SubjectsPortalResults }) => {
             this.results = data.subjectsPortalResults;
+        });
+        this.route.queryParamMap.subscribe((queryParamMap) => {
+            const { pageIndex } = parseSearchQueryParams(queryParamMap);
+            if (pageIndex === 0) {
+                this.isExpanded = true;
+            } else {
+                this.isExpanded = false;
+            }
         });
     }
 }
