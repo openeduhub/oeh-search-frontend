fcade139702a2250b2bbe4419d7ae2d924d02e89
Christopher Lübbemeier <luebbemeier@metaventis.com>
2020-07-23 16:39:48 +0200

    remove 'stage' environment

--- src/environments/environment.stage.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    production: true,
-    relayUrl: 'https://staging.wirlernenonline.de/relay',
-    editorBackendUrl: 'https://staging.wirlernenonline.de/editor',
-    openId: {
-        issuer: 'https://idm.wirlernenonline.de/auth/realms/master',
-        clientId: 'oeh-search-frontend',
-    },
-};
