fdd74fd7e7c8e4c25fb7ae9f9fb304446433d1c7
Christopher Lübbemeier <luebbemeier@metaventis.com>
2020-10-12 15:05:58 +0200

    chore: warn on many change detection runs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,41 @@
-import { Component } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { Component, DoCheck, NgZone } from '@angular/core';
 import {
-    Router,
-    Scroll,
+    NavigationCancel,
+    NavigationEnd,
     NavigationError,
     NavigationStart,
-    NavigationEnd,
-    NavigationCancel,
+    Router,
+    Scroll,
 } from '@angular/router';
-import { filter, delay } from 'rxjs/operators';
-import { ViewportScroller } from '@angular/common';
-import { ErrorService } from './error.service';
+import { delay, filter } from 'rxjs/operators';
 import { AuthService } from './auth.service';
+import { ErrorService } from './error.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements DoCheck {
+    private static readonly CHECKS_PER_SECOND_WARNING_THRESHOLD = 0;
+    private static readonly CONSECUTIVE_TRANSGRESSION_THRESHOLD = 10;
+
     loading = false;
+    private numberOfChecks = 0;
+    private consecutiveTransgression = 0;
+    private checksMonitorInterval: number;
 
     constructor(
         router: Router,
         viewportScroller: ViewportScroller,
         error: ErrorService,
         auth: AuthService,
+        ngZone: NgZone,
     ) {
+        ngZone.runOutsideAngular(() => {
+            this.checksMonitorInterval = window.setInterval(() => this.monitorChecks(), 1000);
+        });
         // Bootstrap login via OpenID.
         // auth.bootstrap();
         // Recreate scroll restoration behavior of the scrollPositionRestoration option (see
@@ -64,5 +74,30 @@ export class AppComponent {
                 ),
             )
             .subscribe(() => (this.loading = false));
+    }
+
+    ngDoCheck(): void {
+        this.numberOfChecks++;
+    }
+
+    private monitorChecks(): void {
+        // console.log('Change detections run in the past second:', this.numberOfChecks);
+        if (this.numberOfChecks > AppComponent.CHECKS_PER_SECOND_WARNING_THRESHOLD) {
+            this.consecutiveTransgression++;
+            if (this.consecutiveTransgression >= AppComponent.CONSECUTIVE_TRANSGRESSION_THRESHOLD) {
+                console.warn(
+                    'Change detection triggered more than ' +
+                        AppComponent.CHECKS_PER_SECOND_WARNING_THRESHOLD +
+                        ' times per second for the past ' +
+                        AppComponent.CONSECUTIVE_TRANSGRESSION_THRESHOLD +
+                        ' seconds consecutively.' +
+                        ' Not showing any more warnings.',
+                );
+                window.clearInterval(this.checksMonitorInterval);
+            }
+        } else {
+            this.consecutiveTransgression = 0;
+        }
+        this.numberOfChecks = 0;
     }
 }
