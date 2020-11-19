8ad8822069515ee2940d6dd15172f3aca2cf4db8
Christopher Lübbemeier <luebbemeier@metaventis.com>
2020-11-19 17:27:33 +0100

    chore: switch to 'corrected' routing (new default)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -41,7 +41,7 @@ const routes: Routes = [
     { path: 'error', component: ErrorComponent },
 ];
 @NgModule({
-    imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+    imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule],
 })
 export class AppRoutingModule {}
