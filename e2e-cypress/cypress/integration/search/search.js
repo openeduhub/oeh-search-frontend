ab80a06bac917249a7247afeb8763c226c2b9c6e
Christopher Lübbemeier <luebbemeier@metaventis.com>
2020-12-11 12:58:12 +0100

    wait for url

--- e2e-cypress/cypress/integration/search/search.js
+++ e2e-cypress/cypress/integration/search/search.js
@@ -5,8 +5,8 @@ Given('I search for {string}', (searchString) => {
         .find('div[class*="search-field-input-wrapper"]')
         .find('input[type="text"]')
         .type(searchString);
-    // cy.wait('@getRelayData'); // suggestions
 
     cy.get('app-new-search-field').find('button[class*="submit-button"]').click();
-    cy.wait('@getRelayData');
+    cy.url().should('include', searchString);
+    cy.wait('@getRelayData.all');
 });
