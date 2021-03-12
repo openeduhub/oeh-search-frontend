88ad167a92aa3848700aa2a8f1ce73c2e5e9fbf0
Christopher Lübbemeier <luebbemeier@metaventis.com>
2021-03-12 11:40:37 +0100

    feat: adapt header and footer to new design

--- src/app/menubar/menubar.component.ts
+++ src/app/menubar/menubar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject, EMPTY, timer } from 'rxjs';
+import { debounce } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 
 @Component({
@@ -8,7 +10,38 @@ import { environment } from '../../environments/environment';
 })
 export class MenubarComponent {
     readonly wordpressUrl = environment.wordpressUrl;
-    mobileOpen: boolean;
 
-    constructor() {}
+    mobileMenuOpen: boolean;
+    newsSubMenuOpen = new BehaviorSubject(false);
+
+    private mouseOverNewsLink = new BehaviorSubject(false);
+    private isTouchDevice: boolean;
+
+    constructor() {
+        this.isTouchDevice =
+            // From https://stackoverflow.com/a/4819886
+            'ontouchstart' in window ||
+            navigator.maxTouchPoints > 0 ||
+            navigator.msMaxTouchPoints > 0;
+        if (!this.isTouchDevice) {
+            this.mouseOverNewsLink
+                .pipe(debounce((value) => (value ? EMPTY : timer(500))))
+                .subscribe(this.newsSubMenuOpen);
+        }
+    }
+
+    onMouseEnterNews(): void {
+        this.mouseOverNewsLink.next(true);
+    }
+
+    onMouseLeaveNews(): void {
+        this.mouseOverNewsLink.next(false);
+    }
+
+    onNewsLinkClicked(event: MouseEvent): void {
+        if (this.isTouchDevice) {
+            this.newsSubMenuOpen.next(!this.newsSubMenuOpen.value);
+            event.preventDefault();
+        }
+    }
 }
