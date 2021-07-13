5c8f35b685414083e53aa69bba44ed617ce424bb
Christopher Lübbemeier <luebbemeier@metaventis.com>
2021-07-13 16:51:44 +0200

    feat: close dialog on back nav

--- src/app/preview-panel/preview-panel.component.ts
+++ src/app/preview-panel/preview-panel.component.ts
@@ -84,7 +84,9 @@ export class PreviewPanelComponent implements OnDestroy {
             panelClass: 'app-preview-panel-dialog-container',
             backdropClass: 'app-backdrop',
             maxWidth: '',
+            closeOnNavigation: false,
         });
+        this.registerCloseOnBackNav();
         this.dialogRef.beforeClosed().subscribe((dialogResult) => {
             if (dialogResult !== 'mode-change') {
                 this.view.selectItem(null);
@@ -93,6 +95,20 @@ export class PreviewPanelComponent implements OnDestroy {
         this.dialogRef.afterClosed().subscribe(() => {
             this.dialogRef = null;
         });
+    }
+
+    /**
+     * On back navigation while the dialog is open, close the dialog instead of navigating back.
+     */
+    private registerCloseOnBackNav(): void {
+        // Push a dummy state on history to catch the popstate event without triggering navigation.
+        history.pushState(null, null, window.location.href);
+        window.addEventListener('popstate', () => this.closeDialog('back-nav'), { once: true });
+        // Cleanup the dummy state in case the dialog was closed by other means.
+        this.dialogRef
+            .beforeClosed()
+            .pipe(filter((dialogResult) => dialogResult !== 'back-nav'))
+            .subscribe(() => history.back());
     }
 
     close(): void {
