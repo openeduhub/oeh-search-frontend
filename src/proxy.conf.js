7fb8184f024a51f2ba133cd70a56e13958dff167
Christopher Lübbemeier <luebbemeier@metaventis.com>
2021-07-23 09:52:00 +0200

    chore: fix proxy

--- src/proxy.conf.js
+++ src/proxy.conf.js
@@ -1,19 +1,14 @@
 require('dotenv').config();
 
-const PROXY_CONFIG = [
-    // Webpack doesn't like an empty proxy configuration.
-    {
-        context: ['/dummy'],
-        target: '',
-    },
-];
+const PROXY_CONFIG = {};
 
 if (process.env.RELAY_PROXY_URL) {
-    PROXY_CONFIG.push({
-        context: ['/relay'],
-        target: process.env.RELAY_PROXY_URL,
+    // For some reason, the proxy doesn't forwarding the path with the request anymore. Therefore,
+    // we hard-code `/graphql` here for now.
+    PROXY_CONFIG['/relay/graphql'] = {
+        target: process.env.RELAY_PROXY_URL + '/graphql',
         changeOrigin: true,
-    });
+    };
 }
 
 module.exports = PROXY_CONFIG;
