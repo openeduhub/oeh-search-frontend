4fc1ed1180fa11c81edd8cf272a4530c66e25121
steinhaus <steinhaus@edu-sharing.net>
2024-04-24 14:00:26 +0200

    migrated to Angular 16

--- src/app/wlo-search/search/search-page/search-page-resolver.service.ts
+++ src/app/wlo-search/search/search-page/search-page-resolver.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
+import { ActivatedRouteSnapshot } from '@angular/router';
 import { SearchResults } from 'ngx-edu-sharing-api';
 import { Observable } from 'rxjs';
 import { EduSharingService } from '../../core/edu-sharing.service';
 import { SearchParametersService } from '../../core/search-parameters.service';
 
 @Injectable()
-export class SearchPageResolverService implements Resolve<SearchResults> {
+export class SearchPageResolverService  {
     constructor(
         private searchParameters: SearchParametersService,
         private eduSharing: EduSharingService,
