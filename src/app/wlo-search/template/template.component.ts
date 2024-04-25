e58a43556847a0aa90d802ffaa26c5f554608bf4
steinhaus <steinhaus@edu-sharing.net>
2024-04-25 12:26:25 +0200

    mock integration of chips web component

--- src/app/wlo-search/template/template.component.ts
+++ src/app/wlo-search/template/template.component.ts
@@ -1,12 +1,13 @@
 import {Component} from '@angular/core';
 import {SharedModule} from "../shared/shared.module";
 import {
+    CollectionChipsComponent,
     UserConfigurableComponent
 } from "wlo-pages-lib";
 
 @Component({
     standalone: true,
-    imports: [SharedModule, UserConfigurableComponent],
+    imports: [SharedModule, UserConfigurableComponent, CollectionChipsComponent],
     selector: 'app-template',
     templateUrl: './template.component.html',
     styleUrls: ['./template.component.scss'],
