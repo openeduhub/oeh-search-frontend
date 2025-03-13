a27bbfd2ab211952e5183914f60667e950fb178e
tkubica-edu <kubica@edu-sharing.net>
2025-03-13 13:11:46 +0100

    chore: add types to filter-swimlane-type pipe, remove pure as it is set as default + add doc string

--- src/app/wlo-search/template/shared/pipes/filter-swimlane-type.pipe.ts
+++ src/app/wlo-search/template/shared/pipes/filter-swimlane-type.pipe.ts
@@ -1,15 +1,23 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Swimlane } from '../types/swimlane';
 
 @Pipe({
     name: 'filterSwimlaneType',
     standalone: true,
-    pure: true,
 })
 export class FilterSwimlaneTypePipe implements PipeTransform {
-    transform(swimlanes: any[], type: string, trigger: number): any[] {
+    /**
+     * Filters a given array of swimlanes by a given type.
+     *
+     * @param swimlanes
+     * @param type
+     * @param trigger
+     */
+    transform(swimlanes: Swimlane[], type: string, trigger: number): Swimlane[] {
+        // trigger is used to prevent usage of pure: false
         if (!swimlanes?.length || !trigger) {
             return [];
         }
-        return swimlanes.filter((swimlane) => swimlane.type === type);
+        return swimlanes.filter((swimlane: Swimlane): boolean => swimlane.type === type);
     }
 }
